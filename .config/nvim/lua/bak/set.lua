bc9021f6fdf6b3dc868a0885904fac796c7e29d3
Justin Bak <tkdefender@protonmail.com>
2023-06-14 15:24:22 -0600

    added remaps and settings

--- .config/nvim/lua/bak/set.lua
+++ .config/nvim/lua/bak/set.lua
@@ -3,12 +3,21 @@ vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
+vim.opt.swapfile = false
+
 vim.opt.smartindent = true
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
 
 vim.opt.wrap = false
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "120"
+
+vim.opt.termguicolors = true
 
 vim.opt.colorcolumn = "120"
