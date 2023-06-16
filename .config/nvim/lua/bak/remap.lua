4d8ded882e62aec75fff1502e18d48257d4a69d1
Justin Bak <tkdefender@protonmail.com>
2023-06-16 12:15:31 -0600

    added new remaps

--- .config/nvim/lua/bak/remap.lua
+++ .config/nvim/lua/bak/remap.lua
@@ -1,5 +1,13 @@
 vim.g.mapleader = " "
 
+local nmap = function(keys, func, desc)
+    if desc then
+        desc = 'LSP: ' .. desc
+    end
+
+    vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+end
+
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("v", "<leader>y", "\"+y")
@@ -14,6 +22,11 @@ vim.keymap.set("n", "N", "Nzzzv")
 
 vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+nmap("<leader>sw", require('telescope.builtin').grep_string, '[S]earch current [W]ord')
+nmap("<leader>sg", require('telescope.builtin').live_grep, '[S]earch by [G]rep' )
+nmap("<leader>ws", require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+nmap("gD", vim.lsp.buf.declaration, '[G]oto [D]eclaration' )
 
 vim.keymap.set("n", "<leader>f", function()
     vim.lsp.buf.format()
